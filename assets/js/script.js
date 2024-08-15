4b0ba02b7d00d07f9570d9d17fe8ba42348a4bf1
Samuel Senesie <samuelsenesie@gmail.com>
2024-08-15 23:41:30 +0000

    Apply style for the popup windows and some HTML code cleanup

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+// Add event listners
+document.addEventListener('DOMContentLoaded', () => {
+    let levelSelection = document.getElementById('level-selection');
+    let subjectSelection = document.getElementById('subject-selection');
+    let yearSelection = document.getElementById('year-selection');
+    let questionContainer = document.getElementById('question-container');
+    let timerBar = document.getElementById('timer-bar');
+    let results = document.getElementById('results');
+})
