06da45f7af8b1635948575d6dae2e6b56ef808bb
Samuel Senesie <samuelsenesie@gmail.com>
2024-08-16 11:50:44 +0000

    Add button elements and event listeners for About Game popup window and aplly additional style to Instructions and Rules popup windows

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,60 @@ document.addEventListener('DOMContentLoaded', () => {
     let timerBar = document.getElementById('timer-bar');
     let results = document.getElementById('results');
 })
+
+
+// Call popup window for "About Game"
+document.getElementById('about-the-game').addEventListener('click', () => {
+    document.getElementById('about-popup').style.display = 'flex';
+})
+
+// Close popup window for "About Game"
+document.getElementById('close-about-game').addEventListener('click', () => {
+    document.getElementById('about-popup').style.display = 'none';
+})
+
+
+// Call popup window for "Instructions"
+document.getElementById('instructions').addEventListener('click', () => {
+    document.getElementById('instructions-popup').style.display = 'flex';
+})
+
+// Close popup window for "Instructions"
+document.getElementById('back-to-about-game').addEventListener('click', () => {
+    document.getElementById('instructions-popup').style.display = 'none';
+    document.getElementById('about-popup').style.display = 'flex';
+})
+
+// Call popup window for "Game Rules"
+document.getElementById('rules').addEventListener('click', () => {
+    document.getElementById('game-rules-popup').style.display = 'flex';
+})
+
+// Close popup window for "Game Rules"
+document.getElementById('back-to-about-rules').addEventListener('click', () => {
+    document.getElementById('game-rules-popup').style.display = 'none';
+    document.getElementById('about-popup').style.display = 'flex';
+})
+
+
+
+
+// Make popup window display at the centre of the screen
+
+function openPopup(url, popupWidth, popHeight) {
+    /*const screenWidth = window.screen.widdth;
+    const screenHeight = window.screen.height;
+
+    const left = (screen.width / 2) - (popupWidth / 2);
+    const top = (screen.height / 2) - (popHeight / 2); */
+
+    const left = (screen.width - width) / 2;
+    const top = (screen.height - height) / 2;
+
+
+    window.open(
+        url,
+        'popupWindow',
+        `width=${width}, height=${height}, top=${top}, left=${left}, scrollbars=yes, resizable=yes`
+    );
+}
