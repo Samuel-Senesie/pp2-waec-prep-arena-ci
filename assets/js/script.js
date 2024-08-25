0cfc1b2515547e112768092de9269dbfbb134398
Samuel Senesie <samuelsenesie@gmail.com>
2024-08-25 12:51:08 +0000

    Clean up code

--- assets/js/script.js
+++ assets/js/script.js
@@ -80,49 +80,59 @@ const questionBank = {
 
 };
 
+// Global variables
+let userName;
+let leaderboardData = []; // store leaderboard entries
+let questions = [];
+let currentQuestionIndex = 0;
+let timeInterval;
+let userAnswers = []; //string to store user's answers for review
+let timeLeft = 60;
+let isPaused = false;
+let scoreBoard = { correct: 0, wrong: 0, unanswered: 0, remaining: 60 };
+
 
 // Add event listeners
 document.addEventListener('DOMContentLoaded', () => {
-    let aboutTheGame = document.getElementById('about-the-game');
-    let startGame = document.getElementById('start-game');
-    let closeAboutGame = document.getElementById('close-about-game');
-    let instructions = document.getElementById('instructions');
-    let backToAboutGame = document.getElementById('back-to-about-game');
-    let rules = document.getElementById('rules');
-    let backToAboutRules = document.getElementById('back-to-about-rules');
-    let backToHomePage = document.getElementById('back-to-home-page');
-    let levelSelection = document.getElementById('level-selection');
-    let jssLevel = document.getElementById('jss-level');
-    let sssLevel = document.getElementById('sss-level');
-    let subjectSelection = document.getElementById('subject-selection');
-    let subjectSelectionJss = document.getElementById('subject-selection-jss');
-    let subjectSelectionSss = document.getElementById('subject-selection-sss');
-    let backJss = document.getElementById('back-jss');
-    let backSss = document.getElementById('back-sss');
-    let yearSelection = document.getElementById('year-selection');
-    let yearDropdown = document.getElementById('year-dropdown');
-    let testSection = document.getElementById('test-section');
-    let questionContainer = document.getElementById('question-container');
-    let subjectButton = document.querySelectorAll('.subject-button');
-    let backToSubjects =document.getElementById('back-to-subjects');
-    let selectedSubjectText = document.getElementById('selected-subject');
-    let startTest = document.getElementById('start-test');
-    let optionsContainer = document.getElementById('options-container');
-    let timerBar = document.getElementById('timer-bar');
-    let results = document.getElementById('results');
-    let nextQuestion = document.getElementById('next-question');
-    let pause = document.getElementById('pause');
-    let resume = document.getElementById('resume');
-    let scoreBoard = {correct: 0, wrong: 0, unanswered: 0, remaining: 60}
-    let reviewSection = document.getElementById('review-section');
+    const startGame = document.getElementById('start-game');
+    const usernameInput = document.getElementById('username-input');
+    const usernameContainer = document.getElementById('username-container');
+    const leaderboardButton = document.getElementById('leaderboard');
+    const leaderboardSection = document.getElementById('leaderboard-section');
+    /*const leaderboardTable = document.getElementById('leaderboard-table').getElementsByTagName('tbody')[0];*/
+    const clearLeaderboard = document.getElementById('clear-leaderboard');
+    const backToLevelSelection = document.getElementById('back-to-level-selection');
+    const aboutTheGame = document.getElementById('about-the-game');
+    const closeAboutGame = document.getElementById('close-about-game');
+    const instructions = document.getElementById('instructions');
+    const backToAboutGame = document.getElementById('back-to-about-game');
+    const rules = document.getElementById('rules');
+    const backToAboutRules = document.getElementById('back-to-about-rules');
+    const backToHomePage = document.getElementById('back-to-home-page');
+    const levelSelection = document.getElementById('level-selection');
+    const jssLevel = document.getElementById('jss-level');
+    const sssLevel = document.getElementById('sss-level');
+    const subjectSelectionJss = document.getElementById('subject-selection-jss');
+    const subjectSelectionSss = document.getElementById('subject-selection-sss');
+    const backJss = document.getElementById('back-jss');
+    const backSss = document.getElementById('back-sss');
+    const yearSelection = document.getElementById('year-selection');
+    const yearDropdown = document.getElementById('year-dropdown');
+    const testSection = document.getElementById('test-section');
+    const questionContainer = document.getElementById('question-container');
+    const optionsContainer = document.getElementById('options-container');
+    const timerBar = document.getElementById('timer-bar');
+    const results = document.getElementById('results');
+    const nextQuestion = document.getElementById('next-question');
+    const pause = document.getElementById('pause');
+    const resume = document.getElementById('resume');
+    const reviewSection = document.getElementById('review-section');
+    const subjectButtons = document.querySelectorAll('.subject-btn');
+    const backToSubjects = document.getElementById('back-to-subjects');
+    const selectedSubjectText = document.getElementById('selected-subject');
+    const startTest = document.getElementById('start-test');
+    
 
-    //To store questions and track current ones
-    let questions = [];
-    let currentQuestionIndex = 0;
-    let timeInterval;
-    let userAnswers = []; //sting to store user's answeres for review
-    let timeLeft = 60;
-    let isPaused = false;
 
     // To navigate to test page
 
@@ -132,6 +142,39 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 
+    if (usernameInput) {
+        usernameInput.addEventListener('input', (e) => {
+            const username = e.target.value.trim();
+            if (username !== '') {
+                localStorage.setItem('username', username);
+            }
+        });
+    }
+
+    if (leaderboardButton) {
+        leaderboardButton.addEventListener('click', () => {
+            levelSelection.style.display = 'none';
+            leaderboardSection.style.display = 'flex';
+            updateLeaderboardSection();
+        });
+    }
+
+    if (backToLevelSelection) {
+        backToLevelSelection.addEventListener('click', () => {
+            leaderboardSection.style.display = 'none';
+            levelSelection.style.display = 'flex';
+        });
+    }
+
+    // Clear leaderboard
+
+    if (clearLeaderboard) {
+        clearLeaderboard.addEventListener('click', () => {
+            leaderboardData = [];
+            localStorage.removeItem('leaderboardData');
+            updateLeaderboardSection();
+        });
+    }
 
     // Display popup window for "About Game"
     if (aboutTheGame) {
@@ -212,7 +255,8 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 
-    document.querySelectorAll('.subject-btn').forEach(btn => {
+    // Display year selection dropdown
+    subjectButtons.forEach(btn => {
         btn.addEventListener('click', () => {
             subjectSelectionJss.style.display = 'none';
             subjectSelectionSss.style.display = 'none';
@@ -220,22 +264,25 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 
+    // Back to subject selection from year selection
     if (backToSubjects) {
         backToSubjects.addEventListener('click', () => {
             yearSelection.style.display = 'none';
             if (subjectSelectionJss.style.display === 'none') {
-                subjectSelectionJss.style.display = 'flex';
-            } else {
                 subjectSelectionSss.style.display = 'flex';
+            } else {
+                subjectSelectionJss.style.display = 'flex';
             }
         });
     }
 
+
+    // Start test
     startTest.addEventListener('click', () => {
-        if (yearDropdown.value !== " ") {
-            let level = 'jss';
-            let subject = 'Language Arts';
-            let year = yearDropdown.value;
+        if (yearDropdown.value.trim() !== " ") {
+            const level = 'jss';
+            const subject = 'Language Arts';
+            const year = yearDropdown.value;
 
             questions = questionBank[level][subject][year];
             scoreBoard.remaining = questions.length;
@@ -253,6 +300,28 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 
+    // Update leaderboard section
+    function updateLeaderboardSection() {
+        leaderboardTable.innerHTML = '';
+        leaderboardData.forEach((entry, index) => {
+            const row = leaderboardTable.insertRow(index);
+            row.insertCell(0).textContent = entry.name;
+            row.insertCell(1).textContent = entry.level;
+            row.insertCell(2).textContent = entry.subject;
+            row.insertCell(3).textContent = entry.score;
+        });
+    }
+
+    // Add score to leaderboard
+    function addScoreToLeaderBoard(name, level, subject, score) {
+        if (name && level && subject && score !== undefined) {
+            leaderboardData.push({name, level, subject, score});
+        }
+        localStorage.setItem('leaderboardData', JSON.stringify(leaderboardData));
+        updateLeaderboardSection();
+    }
+
+    // Update score display
     function updateScoreDisplay() {
         document.getElementById('correct-score').textContent = scoreBoard.correct;
         document.getElementById('wrong-score').textContent = scoreBoard.wrong;
@@ -260,14 +329,16 @@ document.addEventListener('DOMContentLoaded', () => {
         document.getElementById('remaining-questions').textContent = scoreBoard.remaining;
     }
 
+
+    // Display questions
     function displayQuestion() {
         //Load current question from question bank
-        let currentQuestion = questions[currentQuestionIndex];
+        const currentQuestion = questions[currentQuestionIndex];
         //call question text
         questionContainer.querySelector('#question').textContent = currentQuestion.question;
 
         const optionPrefixes = ['A', 'B', 'C', 'D'];
-        let optionButtons = optionsContainer.querySelectorAll('.option-btn');
+        const optionButtons = optionsContainer.querySelectorAll('.option-btn');
         optionButtons.forEach((btn, index) => {
             btn.innerHTML = `<strong>${optionPrefixes[index]}.</strong> ${currentQuestion.options[index]}`;
             /*btn.textContent = `${optionPrefixes[index]}. ${currentQuestion.options[index]}`;*/ //currentQuestion.options[index];
@@ -280,6 +351,7 @@ document.addEventListener('DOMContentLoaded', () => {
         resetTimer();
     }
 
+    // Display inpage notifications
     function showNotification(type, message) {
         const notification = document.createElement('div');
         notification.classList.add('notification', type);
@@ -298,6 +370,7 @@ document.addEventListener('DOMContentLoaded', () => {
         }, 2000);
     }
 
+    // Hide inpage notifications
     function hideNotification(notification) {
         notification.style.opacity = '0';
         setTimeout(() => {
@@ -306,8 +379,9 @@ document.addEventListener('DOMContentLoaded', () => {
         }, 500);
     }
 
+    // Check answer
     function checkAnswer(selectedAnswer) {
-        let currentQuestion = questions[currentQuestionIndex];
+        const currentQuestion = questions[currentQuestionIndex];
         if (selectedAnswer === currentQuestion.correctAnswer) {
             scoreBoard.correct++;
             showNotification('success', 'Correct!');
@@ -332,6 +406,7 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
+    // Load next question
     nextQuestion.addEventListener('click', () => {
         userAnswers[currentQuestionIndex] = 'Skipped';
         checkAnswer('skipped');
@@ -348,13 +423,15 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 
+    // Pause test
     pause.addEventListener('click', () => {
         isPaused = true;
         questionContainer.style.display = 'none';
         optionsContainer.style.display = 'none';
-        showNotification('You paused test')
+        showNotification('info', 'You paused test');
     });
 
+    // Resume test
     resume.addEventListener('click', () => {
         isPaused = false;
         questionContainer.style.display = 'block';
@@ -362,6 +439,7 @@ document.addEventListener('DOMContentLoaded', () => {
         showNotification('info', 'You resumed test.');
     });
 
+    // Start timer
     function startTimer() {
         timeLeft = 60;
         timerBar.style.width = '100%';
@@ -386,29 +464,26 @@ document.addEventListener('DOMContentLoaded', () => {
         }, 1000);
     }
     
+    // Reset timer
     function resetTimer() {
         clearInterval(timeInterval);
         startTimer();
     }
 
 
-
+    //Display review section
     function displayReview() {
         testSection.style.display = 'none';
         reviewSection.style.display = 'block';
         reviewSection.innerHTML = '';
 
         questions.forEach((question, index) => {
-            let userAnswer = userAnswers[index] || 'Not Answered';
-            let correctAnswer = question.correctAnswer;
+            const userAnswer = userAnswers[index] || 'Not Answered';
+            const correctAnswer = question.correctAnswer;
 
-            let answerColor;
-            if (userAnswer === 'Not Answered' || userAnswer === 'Skipped') {
-                answerColor = 'orange';
-            } else {
-                answerColor = userAnswer === correctAnswer ? 'green' : 'red';
-            }
-            let questionHTML = `<div class="review-question">
+            const answerColor = userAnswer === 'Not Answered' || userAnswer === 'Skipped' ? 'orange' : userAnswer === correctAnswer ? 'green' : 'red';
+            
+            const questionHTML = `<div class="review-question">
             <p><strong>Q${index + 1}:</strong> ${question.question}</p>
             <p><strong>Your Answer:</strong> <span style="color: ${answerColor}">${userAnswer}</span></p>
             <p><strong>Correct Answer:</strong> <span style="color: green">${correctAnswer}</span></p>
@@ -422,5 +497,5 @@ document.addEventListener('DOMContentLoaded', () => {
             location.reload();
         });
     }
-
 });
+
